1784ff3b8a155b5a1f6768fe6b40dcb1891b6c4b
davemaxwell612 <davemaxwell612@gmail.com>
2022-04-21 10:05:02 +0100

    Layout edited

--- src/components/MainPostSection.vue
+++ src/components/MainPostSection.vue
@@ -1,27 +1,31 @@
 <template>  
 
-<div class="flex">
-    
-</div>
 <div class="flex-grow pb-3"><p class="text-2xl">Technology Feeds</p></div>
 
     <div class="px-5 pb-8 container">
-    <div class="bg-white rounded-lg p-4 justify-center">
-        <div class="grid grid-cols-2 md:grid-cols-1 lg:grid-cols-2 gap-6">
+    <div class="bg-white rounded-lg p-4">
 
-            <div class="auto-cols-max ">
+        <div class="flex flex-row gap-8">
 
-                <h1>Hello Main</h1>
-
+            <div class="basis-1/2">
+                <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div class="md:flex">
+                    <div class="md:shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="../assets/img.png" alt="Man looking at item at a store">
+                    </div>
+                    <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+                    <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+                    </div>
+                </div>
+                </div>
             </div>
-
-            <div class="w-60">
-
-                <h1>side</h1>
-
+            <div class="basis-1/4">
+                <h2>Hello Side</h2>
             </div>
-                    
         </div>
+        
      </div>
     </div>
 </template>
