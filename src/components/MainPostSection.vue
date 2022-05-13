2d9baaad98f939c6b66c5fb0cdce744d55a4f236
davemaxwell612 <davemaxwell612@gmail.com>
2022-05-13 20:26:48 +0100

    Main post layout updatted

--- src/components/MainPostSection.vue
+++ src/components/MainPostSection.vue
@@ -4,34 +4,64 @@
 
     <div class="px-5 pb-8 container">
     <div class="bg-white rounded-lg p-4">
+      <div class="flex flex-wrap mb-4">
+<!-- =============================================Block Columns for the main posts =============================-->
 
-        <div class="flex flex-wrap mb-4">
+            <div class="w-full lg:w-3/4 mb-7 p-2">
 
-        <div class="w-full lg:w-3/4 mb-7 p-6">
-         <div class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-         <div class="md:flex">
-            <div class="md:shrink-0">
-               <img class="h-28 w-full object-cover md:h-full md:w-48" src="../assets/img.png" alt="Man looking at item at a store">
+              <div class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-6">
+               <div class="flex">
+                 <div class="shrink-0">
+                    <img class="h-28 w-full object-cover md:h-full md:w-28" src="../assets/img.png" alt="Man looking at item at a store">
+                 </div>
+                 <div class="pl-4 pt-2">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Programming</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+                    <!-- <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work.</p> -->
+                 </div>
+               </div>
+              </div>
+
+               <div class="max-w-md bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mb-6">
+               <div class="flex">
+                 <div class="shrink-0">
+                    <img class="h-28 w-full object-cover md:h-full md:w-28" src="../assets/img.png" alt="Man looking at item at a store">
+                 </div>
+                 <div class="pl-4 pt-2">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Entertainment</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
+                    <!-- <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work.</p> -->
+                 </div>
+               </div>
+              </div>
+
             </div>
-            <div class="p-8">
-               <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
-               <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Finding customers for your new business</a>
-               <!-- <p class="mt-2 text-slate-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p> -->
-            </div>
-         </div>
-         </div>
-      </div>
 
-      <div class="w-full md:w-1/2 lg:w-1/4 p-6">
-         <div class="border h-12 text-sm text-grey-dark flex items-center justify-center">
-            <p>Second Div</p>
+      
+<!-- =========================================Sid Bar=================================== -->
+
+         <!-- <div class="w-full md:w-1/2 lg:w-1/4 p-6">
+         <div class="grid grid-rows">
+            <div class="trending">
+            <div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+            <div class="shrink-0">
+                <img class="h-12 w-12" src="../assets/img.png" alt="post img">
+            </div>
+            <div>
+                <div class="text-xl font-medium text-black">The Latest News...</div>
+                <p class="text-slate-500">Read this if you want to!</p>
+            </div>
+            </div>
+        </div>
+        
          </div>
-      </div>
+    </div> -->
       
    </div>
 
-     </div>
-    </div>
+   </div>
+</div>
+
 </template>
 
 <script>
@@ -47,4 +77,5 @@ export default {
 /*
 https://v1.tailwindcss.com/components/flexbox-grids
 https://codepen.io/ScottWhittaker/pen/vrymgg
+https://tailwind-elements.com/docs/standard/components/carousel/
 /*
