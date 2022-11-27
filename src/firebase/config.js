16722f9527e3963669a6dc7e79613b38e2bb4245
David Maxwell <davemaxwell612@gmail.com>
2022-11-27 15:53:36 +0100

    revert changes

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCgHMUWH4ZpZXLxrp_bMrU8beEKpKGS1Hs",
+    authDomain: "feed-arena.firebaseapp.com",
+    projectId: "feed-arena",
+    storageBucket: "feed-arena.appspot.com",
+    messagingSenderId: "432806169989",
+    appId: "1:432806169989:web:f87c9aad64aa80333888ec"
+  };
+
+  //init app
+  firebase.initializeApp(firebaseConfig)
+
+  //init firestore
+  const projectFirestore = firebase.firestore()
+
+  export {projectFirestore}
